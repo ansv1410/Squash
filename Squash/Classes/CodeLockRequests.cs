fb1a861b0a12f9178003283c3c8084ec6068170d
mNesterud <mnesterud@gmail.com>
2016-04-06 14:45:39 +0200

    samtliga databastabeller har fått motsv Class

---
+++ Squash/Classes/CodeLockRequests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Squash.Classes
+{
+    public class CodeLockRequests
+    {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public DateTime DateOfRequest { get; set; }
+
+    }
+}
---
+++ Squash/Classes/MemberCompany.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Squash.Classes
+{
+    public class MemberCompany
+    {
+        public int MemberId { get; set; }
+        public int CompanyId { get; set; }
+
+    }
+}
