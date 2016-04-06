e52a466e82fd2b63b4dfe8c8e5faf675228bfcf3
mNesterud <mnesterud@gmail.com>
2016-04-06 14:46:33 +0200

    +untracked files

---
+++ Squash/Classes/Subscriptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Squash.Classes
+{
+    public class Subscriptions
+    {
+        public int CourtId { get; set; }
+        public int CourtTimeId { get; set; }
+        public int DayId { get; set; }
+        public int MemberId { get; set; }
+
+    }
+}
---
+++ Squash/Classes/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Squash.Classes
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string Phone { get; set; }
+        public string EMail { get; set; }
+        public string StreatAddress { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string IPAddress { get; set; }
+        public string Password { get; set; }
+        public string Cellular { get; set; }
+        public int PublicAddres { get; set; }
+
+    }
+}
