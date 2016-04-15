2f26b27f61194c74f28c9db764923c22baf7d912
mNesterud <mnesterud@gmail.com>
2016-04-15 13:55:14 +0200

    merge

---
+++ Squash/Account/MyPage.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Squash.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Diagnostics;
+using Squash.Classes;
+using System.Net;
+using System.IO;
+using System.Globalization;
+
+
+
+namespace Squash.Account
+{
+    public partial class MyPage : System.Web.UI.Page
+    {
+        Members loggedInMember = new Members();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            loggedInMember.MemberId = Convert.ToInt32(Session["MemberId"]);
+                
+            Response.Write("<script>alert('" + loggedInMember.MemberId + "')</script>");
+        }
+    }
+}
