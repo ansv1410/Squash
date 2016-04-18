d28cdd6e2d5d76722c18d6b976440903af5915e7
mNesterud <mnesterud@gmail.com>
2016-04-18 18:00:04 +0200

    Föregående commit "merge" innehåller fungerande Logga ut, text efter person på länk till MyPage och utkommentering av genererad kod i Site.Master som orsakade problem vid åtkomst av element från code behind

--- Squash/Site.Master.designer.cs
+++ Squash/Site.Master.designer.cs
@@ -13,15 +13,6 @@ namespace Squash {
     public partial class SiteMaster {
         
         /// <summary>
-        /// testarn1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl testarn1;
-        
-        /// <summary>
         /// pubAcc control.
         /// </summary>
         /// <remarks>
